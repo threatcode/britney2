9990bebbaf6a379a62d51659a8bcfdd5173583ad
Sébastien Delafond <sdelafond@gmail.com>
2020-02-04 13:03:22 +0000

    Query debci API using our dedicated key (Comment: #1)

--- kali-prepare-data.sh
+++ kali-prepare-data.sh
@@ -13,5 +13,12 @@ touch data/state/piuparts-summary-unstable.json
 
 mkdir -p data/output
 
-# Fetch autopkgtest results
-wget http://autopkgtest.kali.org/data/status/kali-rolling/amd64/packages.json -O data/state/debci.json
+# Fetch autopkgtest results, reading DEBCI_API_KEY from a .env in the
+# same directory.
+# That key needs to be manually pre-generated on the debci instance
+# itself, using the following command (where "britney" is just an
+# arbitrary internal username that debci will associate to that key):
+#
+#   debci api setkey britney
+. $(dirname $(readlink -f $0))/.env
+wget --header="Auth-Key: $DEBCI_API_KEY" http://autopkgtest.kali.org/api/v1/test\?since=$(date +%s --date="15 days ago")  -O data/state/debci.json
