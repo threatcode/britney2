dd469d3aae597a7f68ad86e4c9f3d3a08dffd08f
Kali Master <kalimaster@repo.kali.org>
2022-04-27 06:51:41 +0000

    Don't submit autopkgtest jobs with --no-prepare

--- kali-run-britney.sh
+++ kali-run-britney.sh
@@ -23,10 +23,11 @@ fi
 
 ./britney.py --config kali.conf --distribution kali -v >>$LOGFILE 2>&1
 
-# Push autopkgtest results to debci, reading DEBCI_API_KEY from a .env
-# in the same directory (see comment in kali-prepare-data.sh for
-# details about key generation)
-. $(dirname $(readlink -f $0))/.env
-export DEBCI_API_KEY
-
-./scripts/debci-put.py ./data/output/debci_*.input >> $LOGFILE 2>&1
+if [ "$KALI_PREPARE" = "yes" ]; then
+    # Push autopkgtest results to debci, reading DEBCI_API_KEY from a .env
+    # in the same directory (see comment in kali-prepare-data.sh for
+    # details about key generation)
+    . $(dirname $(readlink -f $0))/.env
+    export DEBCI_API_KEY
+    ./scripts/debci-put.py ./data/output/debci_*.input >> $LOGFILE 2>&1
+fi
